7ab985ea440339ee76ef336656b4949a79df7241
Bhavana Kandalkar <bhawanakandalkar@gmail.com>
2023-03-15 13:34:25 +0530

    Added second name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "Mayur";
-        System.out.println("Hello world!" + name);
+        String name2 = "Bhavana";
+        System.out.println("Hello world!" + name + name2);
+
+
     }
 }
